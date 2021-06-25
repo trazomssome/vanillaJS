9b525675fcab469b1bc89dd3f439c07a3f2b229f
trazomssome <rkdtmdwn@gmail.com>
2021-06-25 23:01:55 +0900

    make music list
    화면에 표시할 추천음악리스트 작성

---
+++ js/music.js
@@ -0,0 +1,70 @@
+const music = [
+  {
+    subject: "Can't Feel My Face",
+    artist: "The Weeknd",
+  },
+  {
+    subject: "Earthquake",
+    artist: "Tyler, The Creator",
+  },
+  {
+    subject: "Earthquake",
+    artist: "Tyler, The Creator",
+  },
+  {
+    subject: "Moonshine",
+    artist: "Bruno Mars",
+  },
+  {
+    subject: "Symphony",
+    artist: "Clean Bendit",
+  },
+  {
+    subject: "My Song Know What You Did In The Dark",
+    artist: "Fall out Boy",
+  },
+  {
+    subject: "Sicko Mode",
+    artist: "Travis Scott",
+  },
+  {
+    subject: "Paris",
+    artist: "The Chainsmokers",
+  },
+  {
+    subject: "Midnight City",
+    artist: "M83",
+  },
+  {
+    subject: "bellyache",
+    artist: "Billie Eilish",
+  },
+  {
+    subject: "Praise The Lord (Da Shine)",
+    artist: "ASAP Rocky",
+  },
+  {
+    subject: "XO TOUR Llif3",
+    artist: "Lil Uzi Vert",
+  },
+  {
+    subject: "Levels",
+    artist: "Avicii",
+  },
+  {
+    subject: "Team",
+    artist: "Lorde",
+  },
+  {
+    subject: "Clarity",
+    artist: "Zedd",
+  },
+  {
+    subject: "Scars To Your Beautiful",
+    artist: "Alessia Cara",
+  },
+  {
+    subject: "BENEE",
+    artist: "Glitter",
+  },
+];
